52afff1839623774348e17e9628a041b4b7259ef
almkuznetsov <almkuznetsov@mail.ru>
2023-04-12 17:21:45 +0300

    half dz3

--- askme_kuznetsov/askme/models.py
+++ askme_kuznetsov/askme/models.py
@@ -1,13 +1,77 @@
-QUESTIONS = [
-    {
-        'id': i,
-        'title': f'Question {i}',
-        'text': f'Text {i}',
-    } for i in range(10)
-]
+from django.db import models
+from django.contrib.auth.models import User
 
-ANSWERS = [
-    {
-        'text': f'Answer text {i}',
-    } for i in range(8)
-]
+class AnswerManager(models.Manager):
+    def get_answer(self, questions):
+        return self.filter(question=questions).order_by('-date')
+
+    def get_answer_for_id(self, id):
+        return self.get(pk=id)
+
+
+class QuestionManager(models.Manager):
+
+    def new_questions(self):
+        return self.order_by('-date')
+
+    def hot_questions(self):
+        return self.order_by()
+
+    def get_tag(self, tag_name):
+        return self.filter(tags__name__iexact=tag_name).order_by('-date')
+
+    def get_question(self, number):
+        return self.filter(pk=number)[0]
+
+
+class Profile(models.Model):
+    #avatar = models.ImageField(null=True, blank=True, upload_to='static/img/', default='static/img/avatar1a.jpg')
+    register_date = models.DateField(null=False, blank=True, auto_now_add=True)
+    user = models.OneToOneField(User, related_name='userprofile', primary_key=True, null=False,
+                                on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField(max_length=1024)
+    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField('Tag')
+    date = models.DateTimeField(blank=True, auto_now_add=True)
+
+    manager = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    id_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    id_user = models.ForeignKey(Profile, null=False, on_delete=models.CASCADE)
+    value = models.BinaryField(default=1)
+
+    def __str__(self):
+        return self.id_user
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Answer(models.Model):
+    text = models.TextField(max_length=1024)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+    date = models.DateTimeField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, related_name='answers',
+                                 on_delete=models.CASCADE)
+
+    manager = AnswerManager()
+
+    def __str__(self):
+        return self.text
--- askme_kuznetsov/askme/views.py
+++ askme_kuznetsov/askme/views.py
@@ -4,9 +4,9 @@ from . import models
 from django.http import HttpResponse
 
 def index(request):
-    question_items = models.QUESTIONS
+    question_items = models.Question.manager.new_questions()
     p = Paginator(question_items, 3)
-    page_num = request.GET.get('page',1)
+    page_num = request.GET.get('page', 1)
     try:
         page = p.page(page_num)
     except EmptyPage:
@@ -15,31 +15,36 @@ def index(request):
     return render(request, 'index.html', context)
 
 def question(request, question_id):
-    answer_items = models.ANSWERS
-    p = Paginator(answer_items, 3)
+    answer_items = models.Answer.manager.get_answer(models.Question.manager.get_question(1))
+    p = Paginator(answer_items, 1)
     page_num = request.GET.get('page', 1)
     try:
         page = p.page(page_num)
     except EmptyPage:
         page = p.page(1)
-    context = {'question': models.QUESTIONS[question_id],
+    context = {'question': models.Question.manager.get_question(1),
                'answers': page}
     return render(request, 'question.html', context)
+
 
 def ask(request):
     return render(request, 'ask.html')
 
+
 def login(request):
     return render(request, 'login.html')
+
 
 def register(request):
     return render(request, 'register.html')
 
+
 def settings(request):
     return render(request,'settings.html')
 
+
 def tag(request):
-    question_items = models.QUESTIONS
+    question_items = models.Question.manager.get_tag('black_jack')
     p = Paginator(question_items, 3)
     page_num = request.GET.get('page',1)
     try:
@@ -49,8 +54,9 @@ def tag(request):
     context = {'questions': page}
     return render(request, 'tag.html', context)
 
+
 def hot(request):
-    question_items = models.QUESTIONS
+    question_items = models.Question.manager.hot_questions()
     p = Paginator(question_items, 3)
     page_num = request.GET.get('page',1)
     try:
